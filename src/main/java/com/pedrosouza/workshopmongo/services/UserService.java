371179099bcd773df14c1a0024930c434c6a58e1
Pedro Souza <pesouza.civil@gmail.com>
2019-07-29 22:02:11 -0300

    Atualização de usuário com PUT

--- src/main/java/com/pedrosouza/workshopmongo/services/UserService.java
+++ src/main/java/com/pedrosouza/workshopmongo/services/UserService.java
@@ -39,6 +39,17 @@ public class UserService {
         repo.delete(id);
     }
 
+    public User update(User obj) {
+        var newObj = findById(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
     public User fromDTO(UserDto objDto) {
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
